28e876daf3da8401f23ccefe5213c74111875769
joepshoulak <your_email@example.com>
2013-05-06 13:49:15 -0500

    problems with strings

---
+++ define.cc
@@ -0,0 +1,17 @@
+#include <string.h> // std::string
+
+using namespace std;
+
+string lookup(string key)
+{
+  string value;
+  value = string + "!";   
+
+  return value;
+}
+
+int main()
+{
+  return 0;
+}
+
---
+++ server.cc
@@ -0,0 +1,11 @@
+#include "define.cc" // lookup
+#include <iostream>  // std::cout
+
+using std::cout;
+
+int main()
+{
+  cout << lookup("Test");
+  return 0;
+}
+

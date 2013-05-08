6bee332ecd1b4e44d9ff5516f235c36e1d3f919e
joepshoulak <joepshoulak@me.com>
2013-05-08 10:20:13 -0500

    comments

--- client.cc
+++ client.cc
@@ -1,15 +1,17 @@
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <iostream>
+// client.cc
+
+#include <sys/types.h>  //
+#include <sys/socket.h> // sockets and stuff
+#include <iostream>     // std::cout
 
 using std::cout;
  
 int main(int argc, char* argv[])
 {
-  if (argc == 2)
-    cout << argv[1] << "\n";
-  else
-    cout << "Usage: dictionary <word>\n";
+  if (argc == 2) // valid arg structure
+    cout << argv[1] << "\n"; // echo
+  else // invalid arg structure
+    cout << "Usage: dictionary <word>\n"; // print error details
   
   return 0;
 }

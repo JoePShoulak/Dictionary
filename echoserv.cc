10bc284fef63fe3ee872b0f98262add4806018cc
joepshoulak <joepshoulak@me.com>
2013-05-09 10:44:20 -0500

    move the correct line this time

--- echoserv.cc
+++ echoserv.cc
@@ -91,6 +91,7 @@ int main(int argc, char *argv[]) {
     /*  Enter an infinite loop to respond
         to client requests and echo input  */
 
+    char lolcat[1024];
     while (1)
 
 	/*  Wait for a connection, then accept() it  */
@@ -99,7 +100,6 @@ int main(int argc, char *argv[]) {
 	    fprintf(stderr, "ECHOSERV: Error calling accept()\n");
 	    exit(EXIT_FAILURE);
 	}
-        char lolcat[1024];
         int amt = read(conn_s, lolcat, 1024);
 
 

df0f69493068d96f6fd4737e66d05dd8ba0a4303
joepshoulak <joepshoulak@me.com>
2013-05-15 21:28:53 -0500

    added notifier for missing words

--- helper.h
+++ helper.h
@@ -22,39 +22,42 @@ void Crash(char errorType[])
 
 void Define(char toDefine[], int sock)
 {
-    string input(toDefine);
-    int conn_s = sock;
-    input = input.substr(0, (input.length()-2));
-    string line;
-    ifstream myfile ("definitions.txt");
-    if (myfile.is_open())
+  string input(toDefine);
+  int conn_s = sock;
+  input = input.substr(0, (input.length()-2));
+  string line;
+  ifstream myfile ("definitions.txt");
+  if (myfile.is_open())
+  {
+    while ( myfile.good() )
     {
-      int it = 0;
-      while ( myfile.good() )
+      getline(myfile,line);
+      unsigned pos = line.find("!@#$");
+      string word(line.substr(0,pos));
+      if (input.compare(word) == 0)
       {
-        it++;
-        getline(myfile,line);
-        unsigned pos = line.find("!@#$");
-        string word(line.substr(0,pos));
-        if (input.compare(word) == 0)
-        {
-          string rest = line.substr(pos+4);
-          int new_pos = rest.find("!@#$");
-          string wordType(rest.substr(0,new_pos));
-          string wordDef(rest.substr(new_pos+4));
-          write(conn_s, "  ", 2);
-          write(conn_s, ("%s", wordType.data()), wordType.length());
-          write(conn_s, ": ", 2);
-          write(conn_s, ("%s", wordDef.data()), wordDef.length());
-          write(conn_s, "\n", 1);
-        }
+        string rest = line.substr(pos+4);
+        int new_pos = rest.find("!@#$");
+        string wordType(rest.substr(0,new_pos));
+        string wordDef(rest.substr(new_pos+4));
+        write(conn_s, "  ", 2);
+        write(conn_s, ("%s", wordType.data()), wordType.length());
+        write(conn_s, ": ", 2);
+        write(conn_s, ("%s", wordDef.data()), wordDef.length());
+        write(conn_s, "\n", 1);
       }
-      myfile.close();
+      else
+      {
+        write(conn_s, "Word not in dictionary.\n", 34);
+        break;
+      }
     }
-    else
-    {
-      char error[] = "dictionary";
-      Crash(error);
-    }
+    myfile.close();
+  }
+  else
+  {
+    char error[] = "dictionary";
+    Crash(error);
+  }
 }
 
